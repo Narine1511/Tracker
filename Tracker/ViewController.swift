3d0c502b673bfaac9b4f670f79d0c85dc350466c
Наринэ <narine.ovsepian@yandex.ru>
2026-07-02 20:34:49 +0700

    no message

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+   override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       /*view.backgroundColor = .ypBlue
+        let logoImage = UIImage(named: "logo")
+        let imageView = UIImageView(image: logoImage)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)])*/
+        
+        
     }
 
 
